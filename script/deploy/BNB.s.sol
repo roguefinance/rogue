d2ff741dc0215dc815587944a30112b9f77dd593
scamilcar <raclimacs@gmail.com>
2023-09-25 17:30:35 +0200

    config

--- script/deploy/BNB.s.sol
+++ script/deploy/BNB.s.sol
@@ -8,8 +8,8 @@ contract Deploy_Bnb is Script {
 
     function run() external {
 
-        address mav = vm.envAddress("MAV_TOKEN_BNB");
-        address endpoint = vm.envAddress("LZ_ENDPOINT_BNB");
+        address mav = vm.envAddress("MAV_TOKEN_BSC");
+        address endpoint = vm.envAddress("LZ_ENDPOINT_BSC");
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
 
         vm.startBroadcast(deployerPrivateKey);
